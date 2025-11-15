d35528843ec618d148e94fc4021d5c070bd97f5a
hihassan1998 <hassanihussain1998@gmail.com>
2025-11-15 14:41:01 +0100

    update: add the nessary changes for auto-deployment and manual option of deployment

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <h1>This text was added in the mounted file to test---</h1>
+      <h1>This text was added ++in the mounted file to test---</h1>
     </>
   )
 }
