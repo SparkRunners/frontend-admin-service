7731b3986e3d19f87c2914bc13531ca9a61ebc33
Melissa Uzum <uzummelisssa@gmail.com>
2026-01-01 14:05:59 +0100

    fixed faulty push

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { Routes, Route } from 'react-router-dom';
-import { GoogleLogin, OAuthCallback } from './components/GoogleLogin';
+import { Routes, Route } from "react-router-dom";
+import AdminLayout from "./components/AdminLayout";
+import ProtectedRoute from "./routes/ProtectedRoute";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import ScootersPage from "./pages/ScootersPage";
+import "./App.css";
 
+export default function App() {
+    return (
+        <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
 
-function MainApp() {
-  const [count, setCount] = useState(0)
+            <Route element={<ProtectedRoute />}>
+                <Route element={<AdminLayout />}>
+                    <Route path="/" element={<Dashboard />} />
+                    <Route path="/scooters" element={<ScootersPage />} />
+                </Route>
+            </Route>
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <h1>This text was added ++in the mounted file to test---</h1>
-
-      {/* OAuth login button/ 
-      redirect to backedn /auth/google then back to FE http://localhost:5173/frontend-admin-service/oauth-callback?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY5MzAzZWYwN2IwM2RmOTNjZjE1MGQyNiIsInVzZXJuYW1lIjoiSGFzc2FuRGV2IiwiZW1haWwiOiJoYXNzYW4xOTk4ZGV2QGdtYWlsLmNvbSIsImlhdCI6MTc2NDc2OTc2MiwiZXhwIjoxNzY0ODU2MTYyfQ.QagtBNnBLl8NN9twO759utTqqRZGiTuHaGt_eBAiKfs */}
-      <div style={{ marginTop: 20 }}>
-        <GoogleLogin />
-      </div>
-
-    </>
-  )
+            <Route path="*" element={<Login />} />
+        </Routes>
+    );
 }
-
-function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<MainApp />} />
-      <Route path="/oauth-callback" element={<OAuthCallback />} />
-    </Routes>
-  );
-}
-
-export default App
